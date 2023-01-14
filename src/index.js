ca445c0bc6d87d7e1de30bf34b2df1252b47c327
harly laguna <dylangalvan136@gmail.com>
2023-01-14 11:55:06 -0700

    nav bar styling and functionality

--- src/index.js
+++ src/index.js
@@ -1,62 +1,96 @@
-import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
-import { Login, Register, Home, Products, Category, Admindash} from './Components/Index';
-import {fetchProducts} from './api/requests.js';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import {
+  Login,
+  Register,
+  Home,
+  Products,
+  Category,
+  Admindash,
+} from "./Components/Index";
+import { fetchProducts } from "./api/requests.js";
+import "./index.css";
 
 const App = () => {
-  
-    const [token, setToken] = useState(
-        window.localStorage.getItem("token") || null
-      );
-    const [allProducts, setAllProducts] = useState([]);
-    const [user, setUser] = useState([]);
-    const [category, setCategory] = useState("athletic");
+  const [token, setToken] = useState(
+    window.localStorage.getItem("token") || null
+  );
+  const [allProducts, setAllProducts] = useState([]);
+  const [user, setUser] = useState([]);
+  const [category, setCategory] = useState("athletic");
 
+  useEffect(() => {
+    const getAllProducts = async () => {
+      try {
+        const products = await fetchProducts();
+        setAllProducts(products.products);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
+    getAllProducts();
+  }, []);
 
-
-      useEffect(()=> {
-          const getAllProducts = async () => {
-            try{
-            const products = await fetchProducts();
-            setAllProducts(products.products);
-            } catch(error) {
-              console.error(error);
-            }
-          }
-  
-          getAllProducts();
-        }, []);
-
-      return (
-        
+  return (
     <BrowserRouter>
-
-<div id = "container">
-    <div id = "nav-bar"> 
-    <div className= "nav-bar-title">Sole Quest Men's Shoes</div>
-    <Link to = "/products">Products</Link>
-    <Link to = "/register">Register</Link>
-    <Link to = "/login">Login</Link>
-    {/* { token && user.isAdmin === true ?*/} <Link to = "/admindash">Admin</Link>
-    {token ? <Link to = "/logout">Logout</Link> : null }
-    </div>
-      <div id = "main-section">
-
-          <Route path = "/" exact><Home allProducts = {allProducts} setCategory = {setCategory}></Home></Route>
-          <Route path = "/products"><Products allProducts={allProducts} user={user} token={token}></Products></Route>
-          <Route path = "/login"><Login setToken = {setToken}></Login></Route>
-          <Route path = "/register"><Register setToken = {setToken}></Register></Route>
+      <div id="container">
+        <div className="nav-bar-title">Sole Quest</div>
+        <div id="nav-bar">
+          <Link id="Home" to="/">
+            <img src="https://img.freepik.com/premium-vector/shoes-logo-template-design-vector_316488-1463.jpg?w=1060" />
+          </Link>
+          <Link id="Products" to="/products">
+            Products
+          </Link>
+          <Link id="Register" to="/register">
+            Register
+          </Link>
+          <Link id="Login" to="/login">
+            Login
+          </Link>
+          {/* { token && user.isAdmin === true ?*/}{" "}
+          <Link to="/admindash">Admin</Link>
+          {token ? <Link to="/logout">Logout</Link> : null}
+        </div>
+        <div id="main-section">
+          <Route path="/" exact>
+            <Home allProducts={allProducts} setCategory={setCategory}></Home>
+          </Route>
+          <Route path="/products">
+            <Products
+              allProducts={allProducts}
+              user={user}
+              token={token}
+            ></Products>
+          </Route>
+          <Route path="/login">
+            <Login setToken={setToken}></Login>
+          </Route>
+          <Route path="/register">
+            <Register setToken={setToken}></Register>
+          </Route>
           {/* <Route path = "/register"><Logout></Logout></Route> */}
-          <Route path = "/admindash"> <Admindash token = {token} allProducts={allProducts}></Admindash></Route>
-          <Route path = "/category"> <Category token = {token} user = {user} category = {category} 
-                        setCategory = {setCategory} allProducts = {allProducts}></Category></Route>
+          <Route path="/admindash">
+            {" "}
+            <Admindash token={token} allProducts={allProducts}></Admindash>
+          </Route>
+          <Route path="/category">
+            {" "}
+            <Category
+              token={token}
+              user={user}
+              category={category}
+              setCategory={setCategory}
+              allProducts={allProducts}
+            ></Category>
+          </Route>
+        </div>
       </div>
-    </div>
-  </BrowserRouter>
-  )
+    </BrowserRouter>
+  );
 };
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<App />);
