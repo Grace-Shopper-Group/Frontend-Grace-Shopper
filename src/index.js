f947c6575f738140e5f6ac33485800228ae08fe1
cardonandrew <cardonandrew0@gmail.com>
2023-01-14 10:44:56 -0700

    feat: admin dashboard / patch product

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
-import { Login, Register, Home, Products} from './Components/Index';
+import { Login, Register, Home, Products, Admindash} from './Components/Index';
 import {fetchProducts} from './api/requests.js';
 
 const App = () => {
@@ -15,7 +15,6 @@ const App = () => {
           const getAllProducts = async () => {
             try{
             const products = await fetchProducts();
-            console.log("products useEffect", products.products)
             setAllProducts(products.products);
             } catch(error) {
               console.error(error);
@@ -35,7 +34,7 @@ const App = () => {
     <Link to = "/products">Products</Link>
     <Link to = "/register">Register</Link>
     <Link to = "/login">Login</Link>
-    { token && user.isAdmin === true ? <Link to = "/admindash">Admin</Link> : null}
+    {/* { token && user.isAdmin === true ?*/} <Link to = "/admindash">Admin</Link>
     {token ? <Link to = "/logout">Logout</Link> : null }
     </div>
       <div id = "main-section">
@@ -45,8 +44,8 @@ const App = () => {
           <Route path = "/login"><Login setToken = {setToken}></Login></Route>
           <Route path = "/register"><Register setToken = {setToken}></Register></Route>
           {/* <Route path = "/register"><Logout></Logout></Route> */}
-          {/* <Route path = "/admindash"> <Admindash token = {token}></Admindash></Route>
-          <Route path = "/category"> <Category token = {token}></Category></Route> */}
+          <Route path = "/admindash"> <Admindash token = {token} allProducts={allProducts}></Admindash></Route>
+          {/* <Route path = "/category"> <Category token = {token}></Category></Route> */}
       </div>
     </div>
   </BrowserRouter>
